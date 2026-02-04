8aedd9b827f8ed6cbd54079a8c986d23933e90cd
MiguelMontoya221004 <mamontoy11@eafit.edu.co>
2026-02-03 21:53:36 -0500

    Productos con precios

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,65 @@
-from django.shortcuts import render 
-from django.http import HttpResponse 
-from django.views.generic import TemplateView # Create your views here. 
-class HomePageView(TemplateView): 
-    template_name = 'pages/home.html' 
-class AboutPageView(TemplateView): 
-    template_name = 'pages/about.html' 
-    def get_context_data(self, **kwargs): 
-        context = super().get_context_data(**kwargs) 
-        context.update({ "title": "About us - Online Store", 
-                        "subtitle": "About us", 
-                        "description": "This is an about page ...", 
-                        "author": "Developed by: Your Name", }) 
+from django.views import View
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Miguel Montoya",
+        })
         return context
+
+
+class Product:
+    products = [
+        {"id":"1", "name":"TV", "description":"Best TV", "price":500},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":999},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":50},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":99}
+    ]
+
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.products
+
+        return render(request, self.template_name, viewData)
+
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+    def get(self, request, id):
+        try:
+            product_id = int(id)
+            if product_id < 1 or product_id > len(Product.products):
+                return HttpResponseRedirect(reverse('home'))
+            product = Product.products[product_id - 1]
+        except (ValueError, IndexError):
+            return HttpResponseRedirect(reverse('home'))
+        
+        viewData = {}
+        viewData["title"] = product["name"] + " - Online Store"
+        viewData["subtitle"] = product["name"] + " - Product information"
+        viewData["product"] = product
+
+        return render(request, self.template_name, viewData)
+    
